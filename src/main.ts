e79e5dee549c70a65c9cfa70c15535657f842ac9
Marcio Souza <marciomultimedia@gmail.com>
2020-10-05 00:05:39 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const serverConfig: { port: number } = config.get('server');
 
   const port = process.env.PORT || serverConfig.port;
-  const app = await NestFactory.create(AppModule);
+
   await app.listen(port);
   logger.log(`Server running on port ${port}`);
 }
